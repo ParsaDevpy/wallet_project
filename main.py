18d9377fc090d969a4b7ba586990521216d16003
ParsaDevpy <gandomyparsa@gmail.com>
2025-11-13 19:01:43 +0330

    update

--- main.py
+++ main.py
@@ -1,40 +1,21 @@
-import requests
 from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication
 from time import sleep
 from PyQt6.QtWidgets import QWidget,QApplication,QLabel
-
-def nobitex(IRT):
-    r = requests.get(f"https://apiv2.nobitex.ir/v3/orderbook/{IRT}IRT")
-    data = r.json()
-
-    if data["status"] == 'ok' :
-        price = data['lastTradePrice']
-        price = price[:-1]
-        
-    else:
-        price = 404
-
-    return price  
-
+from threading import Thread
+from function import nobitex
 
 def update_price():
-    form.label_4.setText(f'{nobitex("USDT")} تومان')
-    form.label_5.setText(f'{nobitex("ETH")} تومان')
-    form.label_6.setText(f'{nobitex("BTC")} تومان')
-    if form.label_7.text() =='done!':
-        form.label_7.setText("succesful!")
-    elif form.label_7.text() == "" or "succesful!":
-        form.label_7.setText("done!")
+    while True:
+        form.label_4.setText(f'{nobitex("USDT")} تومان')
+        form.label_5.setText(f'{nobitex("ETH")} تومان')
+        form.label_6.setText(f'{nobitex("BTC")} تومان')
+        form.label_4.setText(f'{nobitex("USDT")} تومان')
+        form.label_5.setText(f'{nobitex("ETH")} تومان')
+        form.label_6.setText(f'{nobitex("BTC")} تومان')
 
-# def new_window():
-   
-#     window2 = QWidget()
- 
-#     label = QLabel("coming soon.....",parent=window2)
 
-#     window2.show()
-    
+
 
 
 Form, Window = uic.loadUiType("wallet.ui")
@@ -43,15 +24,11 @@ window = Window()
 form = Form()
 form.setupUi(window)
 window.setFixedSize(241,170)
+price_thread = Thread(target=update_price)
+price_thread.start()
 
-form.label_4.setText(f'{nobitex("USDT")} تومان')
-form.label_5.setText(f'{nobitex("ETH")} تومان')
-form.label_6.setText(f'{nobitex("BTC")} تومان')
-form.pushButton_5.clicked.connect(update_price)
 form.comboBox.addItems(['BTCIRT','ETHIRT','USDTIRT'])
-# for i in range(1,5):
-#     butten = getattr(form,f'pushButton_{i}')
-#     butten.clicked.connect(new_window)
+
 
 
 window.show()
