0148feb319d684f191fcb8afd44ae291051d8cab
ParsaDevpy <gandomyparsa@gmail.com>
2025-11-13 19:34:22 +0330

    update

--- main.py
+++ main.py
@@ -4,10 +4,19 @@ from threading import Thread
 from function_class import main_app
 
 
+Form, Window = uic.loadUiType("sign up_in.ui")
+app = QApplication([])
+window = Window()
+form = Form()
+form.setupUi(window)
+window.setFixedSize(241,205)
 
 
 
 
+
+window.show()
+app.exec()
 
 
 
