2f68dfb79114510e8606e3c9e4e13eee2d2995d8
ParsaDevpy <gandomyparsa@gmail.com>
2025-11-13 19:18:04 +0330

    update

--- function_class.py
+++ function_class.py
@@ -1,4 +1,7 @@
 import requests
+from PyQt6.QtWidgets import QApplication
+from threading import Thread
+from PyQt6 import uic
 
 def nobitex(IRT):
     r = requests.get(f"https://apiv2.nobitex.ir/v3/orderbook/{IRT}IRT")
@@ -13,4 +16,46 @@ def nobitex(IRT):
 
     return price 
 
+class main_app(QApplication):
+    def __init__(self, argv):
+        super().__init__(argv)
 
+        def nobitex(IRT):
+            r = requests.get(f"https://apiv2.nobitex.ir/v3/orderbook/{IRT}IRT")
+            data = r.json()
+
+            if data["status"] == 'ok' :
+                price = data['lastTradePrice']
+                price = price[:-1]
+                
+            else:
+                price = 404
+
+            return price 
+
+        def update_price():
+            while True:
+                form.label_4.setText(f'{nobitex("USDT")} تومان')
+                form.label_5.setText(f'{nobitex("ETH")} تومان')
+                form.label_6.setText(f'{nobitex("BTC")} تومان')
+                form.label_4.setText(f'{nobitex("USDT")} تومان')
+                form.label_5.setText(f'{nobitex("ETH")} تومان')
+                form.label_6.setText(f'{nobitex("BTC")} تومان')
+
+
+
+        Form, Window = uic.loadUiType("wallet.ui")
+        app = QApplication([])
+        window = Window()
+        form = Form()
+        form.setupUi(window)
+        window.setFixedSize(241,170)
+        price_thread = Thread(target=update_price)
+        price_thread.start()
+
+        form.comboBox.addItems(['BTCIRT','ETHIRT','USDTIRT'])
+
+
+
+        window.show()
+        app.exec()
--- main.py
+++ main.py
@@ -1,39 +1,8 @@
 from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication
-from time import sleep
-from PyQt6.QtWidgets import QWidget,QApplication,QLabel
 from threading import Thread
-from function_class import nobitex
-class main_app(QApplication):
-    def __init__(self, argv):
-        super().__init__(argv)
+from function_class import main_app
 
-        def update_price():
-            while True:
-                form.label_4.setText(f'{nobitex("USDT")} تومان')
-                form.label_5.setText(f'{nobitex("ETH")} تومان')
-                form.label_6.setText(f'{nobitex("BTC")} تومان')
-                form.label_4.setText(f'{nobitex("USDT")} تومان')
-                form.label_5.setText(f'{nobitex("ETH")} تومان')
-                form.label_6.setText(f'{nobitex("BTC")} تومان')
-
-
-
-        Form, Window = uic.loadUiType("wallet.ui")
-        app = QApplication([])
-        window = Window()
-        form = Form()
-        form.setupUi(window)
-        window.setFixedSize(241,170)
-        price_thread = Thread(target=update_price)
-        price_thread.start()
-
-        form.comboBox.addItems(['BTCIRT','ETHIRT','USDTIRT'])
-
-
-
-        window.show()
-        app.exec()
 
 
 
