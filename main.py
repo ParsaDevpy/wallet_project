8ebe12c1be5c1335d03225f21121be5e7fd56c52
ParsaDevpy <gandomyparsa@gmail.com>
2025-11-15 23:04:50 +0330

    update

--- main.py
+++ main.py
@@ -1,35 +1,41 @@
 from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication
-from threading import Thread
-from function_class import main_app,sign_up_in
+from PyQt6.QtCore import QTimer
+from function_class import main_app,sign_up_in,sign_up,password
 import json
-# global switch
-# switch = False
-# def check(check):
-#     while True:
-#         if check == "sign_up":
-#             global switch
-#             switch = True
-#         if switch:
-#             window1.close()
-#             window2.show()
-         
-# app = QApplication([])
 
-# window1 = sign_up_in()
-# window2 = main_app()
-# check_Thread = Thread(target=check)
-# check_Thread.start()
 
-     
-# with open("word.json","r") as f:
-#     check_json = json.load(f)["check"]
 
-# window1.show()
-# app.exec()
 app = QApplication([])
 
+
+def check():
+    with open("word.json","r") as f:
+        check_json = json.load(f)["check"]
+    if check_json == "sign_up":
+        window1.close()
+        window2.show()  
+      
+
 window1 = sign_up_in()
+window2 = password()
+
+
+timer = QTimer()
+timer.timeout.connect(check)
+timer.start(200)
+     
 
 window1.show()
 app.exec()
+
+
+# form,window = uic.loadUiType("password.ui")
+# app = QApplication([])
+# form = form()
+# window = window()
+# form.setupUi(window)
+# window.setFixedSize(270,220)
+
+# window.show()
+# app.exec()
